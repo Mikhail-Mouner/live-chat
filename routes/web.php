ad9e00872b795c9ef2fa8d527699290ad5d5d873
Mikhail <44241285+Mikhail-Mouner@users.noreply.github.com>
2021-05-02 21:12:37 +0200

    branch[app1_email_verify]

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+Auth::routes(['verify'=>true]);
+
+/*Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');*/
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
